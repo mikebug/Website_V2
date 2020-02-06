2cfbde7b92b53d454d0bb17c6e599619f588ffe8
Michael Buglione <michaelbuglione@Michaels-MacBook-Pro.local>
2020-02-06 10:45:05 -0500

    adding loading

--- js/home.js
+++ js/home.js
@@ -1,30 +1,58 @@
 const element = document.getElementById("body");
 element.classList.add("animated", "fadeInUp");
-document.getElementById("link1").addEventListener("click", function () {
+document.getElementById("link1").addEventListener("click", function() {
   const element = document.getElementById("body");
   element.classList.add("animated", "fadeOutDown");
-  setTimeout(function () {
+  setTimeout(function() {
     window.location.replace("../html/about.html");
   }, 1100);
 });
-document.getElementById("link2").addEventListener("click", function () {
+document.getElementById("link2").addEventListener("click", function() {
   const element = document.getElementById("body");
   element.classList.add("animated", "fadeOutDown");
-  setTimeout(function () {
+  setTimeout(function() {
     window.location.replace("../html/portfolio.html");
   }, 1100);
 });
-document.getElementById("link3").addEventListener("click", function () {
+document.getElementById("link3").addEventListener("click", function() {
   const element = document.getElementById("body");
   element.classList.add("animated", "fadeOutDown");
-  setTimeout(function () {
+  setTimeout(function() {
     window.location.replace("../html/Gallery.html");
   }, 1100);
 });
-document.getElementById("link4").addEventListener("click", function () {
+document.getElementById("link4").addEventListener("click", function() {
   const element = document.getElementById("body");
   element.classList.add("animated", "fadeOutDown");
-  setTimeout(function () {
+  setTimeout(function() {
+    window.location.replace("../html/contact.html");
+  }, 1100);
+});
+document.getElementById("link5").addEventListener("click", function() {
+  const element = document.getElementById("body");
+  element.classList.add("animated", "fadeOutDown");
+  setTimeout(function() {
+    window.location.replace("../html/about.html");
+  }, 1100);
+});
+document.getElementById("link6").addEventListener("click", function() {
+  const element = document.getElementById("body");
+  element.classList.add("animated", "fadeOutDown");
+  setTimeout(function() {
+    window.location.replace("../html/portfolio.html");
+  }, 1100);
+});
+document.getElementById("link7").addEventListener("click", function() {
+  const element = document.getElementById("body");
+  element.classList.add("animated", "fadeOutDown");
+  setTimeout(function() {
+    window.location.replace("../html/Gallery.html");
+  }, 1100);
+});
+document.getElementById("link8").addEventListener("click", function() {
+  const element = document.getElementById("body");
+  element.classList.add("animated", "fadeOutDown");
+  setTimeout(function() {
     window.location.replace("../html/contact.html");
   }, 1100);
 });
