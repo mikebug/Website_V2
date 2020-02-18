9e7ab31bcd02833a75628b8779216ac5efedbcdc
Mike Buglione <mbuglion@stevens.edu>
2020-02-18 00:14:50 -0500

    new ish

--- js/about.js
+++ js/about.js
@@ -5,3 +5,10 @@ document.getElementById("link").addEventListener("click", function() {
     window.location.replace("http://127.0.0.1:5500/html/home.html");
   }, 1100);
 });
+document.getElementById("link").addEventListener("click", function() {
+  const element = document.getElementById("particles-js");
+  element.classList.add("animated", "fadeOutDown");
+  setTimeout(function() {
+    window.location.replace("http://127.0.0.1:5500/html/home.html");
+  }, 1100);
+});
--- js/project_1.js
+++ js/project_1.js
@@ -4,7 +4,7 @@ document.getElementById("link").addEventListener("click", function() {
   const element = document.getElementById("about");
   element.classList.add("animated", "fadeOutDown");
   setTimeout(function() {
-    body.style.backgroundColor = "#0c0c0c";
+    body.style.backgroundColor = "#ffffff";
   }, 100);
   setTimeout(function() {
     window.location.replace("http://127.0.0.1:5500/html/home.html");
